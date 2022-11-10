306b967fab94159c10d92e6b9dbf8815c063ad06
saitek <saitek300@gmail.com>
2022-11-10 08:07:11 -0800

    added a question array

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,37 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'what is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'how would you describe you application?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'how would you install this application?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'how do you use this application?.'
+    },
+    {
+        type: 'list',
+        name: 'contribution',
+        message: 'what are the contribution guidelines?',
+    },
+    {
+        type: 'type',
+        name: 'test',
+        message: 'do you have any test examples written? provide them here: ',
+    },
+
 ];
 
 // TODO: Create a function to write README file
