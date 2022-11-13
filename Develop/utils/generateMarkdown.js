84c0b68c38041201ac0f20a5587bdf1b63c8b3a5
saitek <saitek300@gmail.com>
2022-11-12 16:57:38 -0800

    added readme template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,54 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+    # ${data.title}
+
+    ## Description
+    
+      ${data.description}
+    
+    ## Table of Contents
+    
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+    ## Installation
+    
+    The following dependencies must be installed to use this application ${data.installation}
+    
+    ## Usage
+    in order to use this app, ${data.usage}
+    
+    ## Credits
+    
+      ${data.credit}
+    
+    ## License
+    
+    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+    
+    ---
+    
+    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+    
+    ## Badges
+    
+
+
+    ## Features
+    
+    If your project has a lot of features, list them here.
+    
+    ## How to Contribute
+    
+      ${data.contribution}
+    
+    ## Tests
 
 `;
 }
